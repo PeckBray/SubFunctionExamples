95e72823219a6851f5f09eeabcc0cf954fde6a84
Brayden Peck <brayden.peck2@gmail.com>
2021-01-27 08:50:18 -0700

    add subs, args, function examples

--- SubFunctionExamples.vb
+++ SubFunctionExamples.vb
@@ -1,14 +1,38 @@
 ﻿Module SubFunctionExamples
 
     Sub Main()
-        Console.WriteLine("I'm in sub main!!")
+        'Console.WriteLine("I'm in sub main!!")
+        'MyOtherSub() 'call other sub
+        'Console.WriteLine("I'm back in sub main!!")
+        'MyOtherSub()
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
 
+        firstNumber = 5
+        secondNumber = 6
+        'DoMath(firstNumber)
+        'DoMath(secondNumber)
+        'DoMath(7)
+
+        ' DoMath(firstNumber, secondNumber)
+        Console.WriteLine(MyCoolFunction("Hi"))
 
         Console.Read()
     End Sub
-
+    'Subs execute a block of code 
     Sub MyOtherSub()
-        Console.WriteLine("I'm in my other sub")
-        Console.Read()
+        Console.WriteLine("I'm in my other sub!!")
     End Sub
+    'pass data to a sub/function with arguments
+    Sub DoMath(someNumber As Integer, someOtherNumber As Integer)
+        someNumber *= 2
+        Console.WriteLine(someNumber + someOtherNumber)
+    End Sub
+    'functions execute a block of code and then return data to the caller
+    Function MyCoolFunction(noteToSelf) As String
+        Dim temp As String
+        temp = $"hello, {noteToSelf}"
+        Return temp
+    End Function
+
 End Module

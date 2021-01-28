d9b00764b0c1d73dc8a777ce9e8fc02133656e0a
Brayden Peck <brayden.peck2@gmail.com>
2021-01-28 08:48:37 -0700

    Manipulate strings using subs and functions

---
+++ PromptUser.vb
@@ -0,0 +1,23 @@
+﻿Module PromptUser
+    Sub Main()
+        PromptUser("Hello user")
+
+        For i = 0 To 5
+            StoreMessages("Hello" & vbNewLine) 'makes a new line after the string
+        Next
+        PromptUser(StoreMessages(""))
+        Console.Read()
+    End Sub
+
+    Sub PromptUser(message As String)
+        Console.WriteLine(message)
+    End Sub
+
+    Function StoreMessages(message As String) As String
+        Static storedMessages As String 'static extends the lifetime of the string
+        storedMessages &= message
+
+        Return storedMessages
+    End Function
+
+End Module
